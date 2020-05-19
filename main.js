2ab478b4fc3d265bb1306bbe8d364004c893760e
vahan akopjanian <vahan.bclub@gmail.com>
2020-05-19 14:14:02 +0300

    buildJoke function created

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const update = document.querySelector(".update");
 const jokeId = document.querySelector(".jokeId");
 const jokeCategory = document.querySelector(".joke-category");
 const likeBtn = document.querySelector(".likeBtn");
-let jokeContainer = document.querySelector(".joke-container");
+const jokeContainer = document.querySelector(".joke-container");
 const categories = document.querySelector(".categories");
 //const categoriesBtn = document.querySelector(".categoriesBtn");
 const like = document.querySelector(".liked");
@@ -12,8 +12,10 @@ const randomRadio = document.querySelector('[value="random"]');
 const categoriesRadio = document.querySelector('[value="categories"]');
 const searchRadio = document.querySelector('[value="search"]');
 const searchField = document.querySelector(".searchField");
+const favourites = document.querySelector(".favourites");
 //const [randomRadio, categoriesRadio, searchRadio] = document.querySelectorAll('[name="mode"]');
-
+const likeSvg =
+  '<svg id="Layer_1" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path class="fill" d="m446.51 84.39a134.8 134.8 0 0 1 0 190.65l-190.37 192.06-190.66-190.65a134.8 134.8 0 0 1 189.79-191.47l.59-.59a134.8 134.8 0 0 1 190.65 0z" fill="transparent"/><path d="m351.185 38.9a139.778 139.778 0 0 0 -95.969 37.773 140.782 140.782 0 0 0 -193.982 4.891c-54.89 54.891-54.89 144.221 0 199.133l190.666 190.647a6 6 0 0 0 4.242 1.757h.014a6 6 0 0 0 4.247-1.777l190.346-192.042a140.816 140.816 0 0 0 -99.564-240.382zm91.061 231.915-186.121 187.785-186.404-186.388c-50.213-50.232-50.214-131.95 0-182.162a128.771 128.771 0 0 1 181.346-.8 6 6 0 0 0 8.451-.034l.587-.588a128.808 128.808 0 1 1 182.141 182.186z" fill="#FF6767"/></svg>';
 randomRadio.addEventListener("click", () => {
   categories.innerHTML = "";
   searchField.classList.add("hidden");
@@ -27,10 +29,6 @@ categoriesRadio.addEventListener("click", () => {
 searchRadio.addEventListener("click", () => {
   categories.innerHTML = "";
   searchField.classList.remove("hidden");
-});
-
-like.addEventListener("click", () => {
-  like.classList.toggle("active");
 });
 
 getJokesBtn.addEventListener("click", () => {
@@ -54,16 +52,12 @@ function getJokes(e) {
     })
     .then(function (data) {
       console.log(data.categories);
+      jokeContainer.innerHTML = "";
       if (mode === "search") {
-        data = data.result[Math.floor(Math.random() * data.total)];
-        joke.innerHTML = highlightQuery(data.value, searchField.value);
+        jokeContainer.append(...data.result.map(buildJoke));
       } else {
-        joke.innerText = data.value;
+        jokeContainer.append(buildJoke(data));
       }
-      likeBtn.classList.remove("hidden");
-      jokeId.innerText = `ID: ${data.id}`;
-      update.innerText = data.updated_at;
-      jokeCategory.innerText = data.categories[0] || "";
       jokeContainer.classList.remove("hidden");
     })
     .catch(function (err) {
@@ -102,7 +96,28 @@ function buildJoke(data) {
   wrapper.className = "wrapper";
   const jokeId = document.createElement("div");
   jokeId.className = "jokeID";
+  jokeId.innerText = `ID: ${data.id}`;
   const joke = document.createElement("div");
   joke.className = "joke";
+  const mode = document.querySelector('[name="mode"]:checked').value;
+  if (mode === "search") {
+    joke.innerHTML = highlightQuery(data.value, searchField.value);
+  } else {
+    joke.innerText = data.value;
+  }
+  const like = document.createElement("label");
+  like.className = "like";
+  like.innerHTML = likeSvg;
   const likeBtn = document.createElement("input");
+  likeBtn.type = "checkbox";
+  likeBtn.hidden = true;
+  like.prepend(likeBtn);
+  const jokeUpdate = document.createElement("div");
+  jokeUpdate.className = "update";
+  jokeUpdate.innerText = data.updated_at;
+  const jokeCategory = document.createElement("div");
+  jokeCategory.className = "joke-category";
+  jokeCategory.innerText = data.categories[0] || "";
+  wrapper.append(jokeId, joke, like, jokeUpdate, jokeCategory);
+  return wrapper;
 }
